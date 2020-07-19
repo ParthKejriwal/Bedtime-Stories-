d729b8f96c2ab2ddb5f6fae25f207efd33512190
ParthKejriwal <poorvikejriwal@gmail.com>
2020-07-19 08:55:01 +0530

    Added multiline TextInput Box

--- App.js
+++ App.js
@@ -7,12 +7,12 @@ import Tab2 from "./Tab2";
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Tab1" component={Tab1} />
-          <Tab.Screen name="Tab2" component={Tab2} />
-        </Tab.Navigator>
-      </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Tab.Navigator>
+                <Tab.Screen name="Tab1" component={Tab1}/>
+                <Tab.Screen name="Tab2" component={Tab2}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
--- Tab2.js
+++ Tab2.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 
-export default function Tab2() {
+export default function MultilineTextInput() {
+
+  const [value, onChangeText] = React.useState('Multiline text\nline 2\nline3');
   return (
-    <View style={styles.container}>
-      <Text>I am Tab 2</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <TextInput
+            multiline
+            numberOfLines={16}
+            onChangeText={text => onChangeText(text)}
+            value={value}
+            editable
+            maxLength={1000}
+        />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
+  paragraph: {
+    margin: 24,
+    height: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    justifyContent: 'center'
+  }
 });
