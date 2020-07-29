05b42228eda2e8429f5791e1b2106eea53951400
ParthKejriwal <poorvikejriwal@gmail.com>
2020-07-29 13:48:13 +0530

    I added a screen which shows everything

--- screens/ReadStory.js
+++ screens/ReadStory.js
@@ -1,50 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import db from '../config.js'
+import { Text, View } from 'react-native';
+import db from '../config.js';
+import { FlatList } from 'react-native-gesture-handler';
 
-export default class ReadStory extends React.Component {
-  constructor(props){
-    super(props)
-    this.state={
-      search:'',
-      allStories:[]
+export default class Searchscreen extends React.Component {
+
+constructor(props){
+  super(props)
+  this.state=({allStories:[]})
+}
+
+componentDidMount = async ()=>{ 
+  const query = await db.collection("Stories").get()
+  query.docs.map((doc)=>{
+    this.setState({ allStories: [...this.state.allStories,doc.data()] })
+  })
+}
+
+    render() {
+      return (
+       <FlatList data={this.state.allStories} renderItem={({item})=>(
+        <View>
+        <Text>
+          {'Story Title: ' + item.stortTitle}
+        </Text>
+        <Text>
+          {'Story: ' + item.storyText}
+        </Text>
+        <Text>
+          {'Author: ' + item.author}
+        </Text>
+      </View>
+       )} 
+       keyExtractor={(item,index)=>index.toString()}>
+       </FlatList>
+      );
     }
   }
-  componentDidMount = async ()=>{ 
-    const query = await db.collection("Stories").get()
-    query.docs.map((doc)=>{
-      this.setState({ allStories: [...this.state.allStories,doc.data()] })
-    })
-  }
-
-  updateSearch = (search) => {
-    this.setState({ search });
-  };
-
-  render() {
-    const { search } = this.state;
-
-    return (
-      <SearchBar
-        placeholder="Type Here..."
-        onChangeText={this.updateSearch}
-        value={search}
-        data={this.state.allStories} renderItem={({item})=>(
-          <View>
-          <Text>
-            {'Story Title: ' + item.storyTitle}
-          </Text>
-          <Text>
-            {'Author: ' + item.author}
-          </Text>
-          <Text>
-            {'Story: ' + item.story}
-          </Text>
-        </View>
-         )} 
-         keyExtractor={(item,index)=>index.toString()}>
-         </SearchBar>
-     );
-  }
-}
